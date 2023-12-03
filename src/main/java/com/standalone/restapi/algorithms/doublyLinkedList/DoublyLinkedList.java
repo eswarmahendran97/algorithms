8b5d3955c416b82a090e2c8d8feea81ccfcbef13
Eswaramoorthy <eswarmahendran97@gmail.com>
2023-12-03 17:04:06 +0530

    Doubly linked list

--- src/main/java/com/standalone/restapi/algorithms/doublyLinkedList/DoublyLinkedList.java
+++ src/main/java/com/standalone/restapi/algorithms/doublyLinkedList/DoublyLinkedList.java
@@ -1,13 +1,13 @@
 package com.standalone.restapi.algorithms.doublyLinkedList;
 
-public class DoublyLinkedList {
+public class DoublyLinkedList<E> {
 
-    static class Node {
+    static class Node<E> {
         Node prev;
         Node next;
-        int data;
+        E data;
 
-        Node(int data) {
+        Node(E data) {
             this.data = data;
             this.prev = null;
             this.next = null;
@@ -17,45 +17,58 @@ public class DoublyLinkedList {
     Node head = null;
     Node lastCopy = null;
 
-    public void add(int data) {
-        Node n = new Node(data);
+    public void add(E data) {
+        Node temp = new Node(data);
         if (head == null) {
-            head = lastCopy = n;
+            head = lastCopy = temp;
         } else {
-            lastCopy.next = n;
-            n.prev = lastCopy;
-            lastCopy = n;
+            lastCopy.next = temp;
+            temp.prev = lastCopy;
+            lastCopy = temp;
+        }
+    }
+
+    public void add(int pos, E data) {
+        Node temp = new Node(data);
+        Node current = head;
+        int currPosition = 0;
+
+        while (currPosition < pos) {
+            current = current.next;
+            currPosition++;
+        }
+        if (current != null) {
+            temp.next = current;
+            temp.prev = current.prev;
+            current.prev.next = temp;
+            current.prev = temp;
+        }
+    }
+
+    public void delete(int position) {
+        Node current = head;
+        int currPosition = 0;
+        while (currPosition < position) {
+            current = current.next;
+            currPosition++;
+        }
+        if (current != null) {
+            current.prev.next = current.next;
+            current.next.prev = current.prev;
+            current.prev = current.next = null;
         }
     }
 
     public void display() {
-        Node n = head;
-        while (n != null) {
-            System.out.println(n.data);
-            n = n.next;
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
         }
     }
-
-    public void add(int pos, int data) {
-        Node n = new Node(data);
-        int currentPos = 0;
-        Node current = head;
-
-        while (currentPos != pos) {
-            current = current.next;
-            currentPos++;
-        }
-        if (current != null) {
-            n.next = current;
-            n.prev = current.prev;
-            current.prev = n;
-            current.prev.next = n;
-        }
-    }
-
 
     public static void main(String[] args) {
-        DoublyLinkedList list = new DoublyLinkedList();
+        DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
         list.add(1);
         list.add(2);
         list.add(3);
@@ -63,7 +76,7 @@ public class DoublyLinkedList {
         list.add(5);
         list.add(6);
         list.add(3, 10);
+        list.delete(4);
         list.display();
     }
-
 }
